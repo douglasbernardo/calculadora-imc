3ccd9bf2f78ec09ebd53b1c1c0d5156331a508cf
Douglas Bernardo de Melo <43753293+douglasbernardo@users.noreply.github.com>
2021-05-18 20:33:02 -0300

    Add files via upload
    funções de verificação

--- js/javascript.js
+++ js/javascript.js
@@ -4,10 +4,22 @@ var alturaUsuario = document.getElementById("altura")
 var icone = document.getElementById("igual")
 var resultadoImcUsuario = document.getElementById("resultado")
 
-icone.addEventListener("click",function(){
-    let calculo = pesoUsuario.value/(alturaUsuario.value*alturaUsuario.value)
-    resultadoImcUsuario.value = calculo.toFixed(1)
-    setTimeout(()=>{
-        resultadoImcUsuario.value = ''
-    },8000)
-})
+
+const calculaIMC = () => {
+    icone.addEventListener("click",function(){
+        let calculo = pesoUsuario.value/(alturaUsuario.value*alturaUsuario.value)
+        validaValores(pesoUsuario.value,alturaUsuario.value)
+        resultadoImcUsuario.value = calculo.toFixed(1)
+        window.onload = ()=>{
+            pesoUsuario.value = ''
+            alturaUsuario = ''
+            resultadoImcUsuario.value = ''
+        }
+    })
+}
+
+const validaValores = (pesoUsuario,alturaUsuario)=>{
+
+    if (pesoUsuario || alturaUsuario != '') calculaIMC()
+
+}
